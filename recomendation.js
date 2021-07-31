fd01579b860ef50c471da6bcb09f93051f567434
Dom-DJ <61599006+Dom-DJ@users.noreply.github.com>
2021-07-31 20:00:16 +0100

    remove await

--- recomendation.js
+++ recomendation.js
@@ -62,7 +62,7 @@ for (const [key, value] of Object.entries(file[0])) {
   watched[value] = 0
 }
 
-const tfliteModel = await tflite.loadTFLiteModel(
+const tfliteModel = tflite.loadTFLiteModel(
     "optimised_model.tflite");
 
 function setWatched(num){
